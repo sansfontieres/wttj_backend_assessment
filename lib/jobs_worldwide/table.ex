f26395406b34b4eac5094ac020b5483a0fb20b63
Romain Hervier <romain@sansfontieres.com>
2021-11-18 21:13:55 +0100

    Implemented the table visualization.
    I had to format my data to work with TableRex. Basically, the library
    wants a list for the table header and a list of lists for for rows.
    It introduced some code smell like nested operations, but the code is
    short and easy to parse.

--- lib/jobs_worldwide/table.ex
+++ lib/jobs_worldwide/table.ex
@@ -1,13 +1,45 @@
 defmodule JobsWorldwide.Table do
   @moduledoc "Creates a table out of job listing"
 
-  alias JobsWorldwide.CSVParser
-  alias TableRex.Table
+  # This function formats a list of rows to work with TableRex
+  @spec rowify(map, map, map) :: list
+  defp rowify(continents_totals, categories_totals, frequencies) do
+    totals_row = Map.values(categories_totals)
+    total_offers = Enum.sum(totals_row)
+    totals_row = ["TOTAL", total_offers | totals_row]
+    row_list = Map.keys(categories_totals)
+    header_list = Map.keys(continents_totals)
+
+    rows =
+      [] ++
+        for x <- header_list do
+          row = [x]
+
+          continent_total = continents_totals[x]
+
+          total =
+            for z <- row_list do
+              total = frequencies[{x, z}]
+
+              case total do
+                nil -> 0
+                _ -> total
+              end
+            end
+
+          row = row ++ [continent_total] ++ total
+          row
+        end
+
+    rows = [totals_row | rows]
+    rows
+  end
 
   @doc """
   Returns a table from a list of jobs offers.
   - Y axis cells are categories of jobs.
   - X axis cells are continents of offices.
+  - The first row and columns are the totals of offers
 
   Example:
       iex> Jobs.Worldwide.Table.create_table(jobs_offers)
@@ -19,10 +51,11 @@ defmodule JobsWorldwide.Table do
     continents_totals = job_offers |> Enum.frequencies_by(&elem(&1, 0))
     categories_totals = job_offers |> Enum.frequencies_by(&elem(&1, 1))
 
-    total = length(job_offers)
-    IO.inspect(frequencies)
-    IO.inspect(continents_totals)
-    IO.inspect(categories_totals)
-    IO.inspect(total)
+    # Formatting the totals to fit into TableRex’ needs
+    header = ["", "TOTAL" | Map.keys(categories_totals)]
+    rows = rowify(continents_totals, categories_totals, frequencies)
+
+    TableRex.quick_render!(rows, header)
+    |> IO.puts()
   end
 end

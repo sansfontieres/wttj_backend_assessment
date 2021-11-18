15ab13d38d9f16c736770537a6fc86e546a64b90
Romain Hervier <romain@sansfontieres.com>
2021-11-18 11:35:44 +0100

    init

---
+++ test/wttj_backend_assessment_test.exs
@@ -0,0 +1,8 @@
+defmodule WttjBackendAssessmentTest do
+  use ExUnit.Case
+  doctest WttjBackendAssessment
+
+  test "greets the world" do
+    assert WttjBackendAssessment.hello() == :world
+  end
+end
